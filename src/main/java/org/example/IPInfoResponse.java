80da8170c9116ce3ba9fcc8f863097d8fb6cbba1
Денис Шевченко <Muz1kash1@yandex.ru>
2023-07-07 03:06:54 +0300

    Решил задачу с парсингом и записью айпишника

---
+++ src/main/java/org/example/IPInfoResponse.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class IPInfoResponse {
+  private final String ip;
+
+  public IPInfoResponse(String ip) {
+    this.ip = ip;
+  }
+
+  public String getIp() {
+    return ip;
+  }
+}
+
